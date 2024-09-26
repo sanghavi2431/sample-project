08ff708f712d2f773f21e01fb1a7b23ec00ebb2b
sanghavi2431 <sanghavi2431@gmail.com>
2024-09-26 21:51:11 +0530

    Update BeforeInstallHook.sh

--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -2,5 +2,5 @@
 set -e
 sudo npm update -y
 sudo pm2 update
-sudo aws s3 cp s3://S3-PATH-CONFIG/ecosystem.config.js /home/ubuntu/PROJECT_NAME/
+sudo aws s3 cp s3://codepipeline-us-west-1-276302327401/woloo-project/config.js /home/ubuntu/sample-project/
 sudo service nginx reload
