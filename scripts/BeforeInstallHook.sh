ae993dbb40d0466cb6270fa777c51a8cf2a0d464
sanghavi2431 <sanghavi2431@gmail.com>
2024-10-09 14:40:41 +0530

    Update BeforeInstallHook.sh

--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
+
+# Update npm packages
 sudo npm update -y
+
+# Update PM2 to the latest version
 sudo pm2 update
-sudo aws s3 cp s3://practiceforcicd/practiceforcicd/config.js /home/ubuntu/sample-project/
+
+# Remove existing pm2.config.js if it exists
+if [ -f /home/ubuntu/sample-project/pm2.config.js ]; then
+    echo "Removing existing pm2.config.js"
+    sudo rm /home/ubuntu/sample-project/pm2.config.js
+fi
+
+# Copy the new pm2.config.js from S3
+echo "Copying pm2.config.js from S3"
+sudo aws s3 cp s3://practiceforcicd/practiceforcicd/pm2.config.js /home/ubuntu/sample-project/
+
+# Reload Nginx service
 sudo service nginx reload
