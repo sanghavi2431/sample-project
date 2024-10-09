c52a2ba8d740e53bf223bbb22d50d34d18bb26a9
sanghavi2431 <sanghavi2431@gmail.com>
2024-10-09 14:24:54 +0530

    Update BeforeInstallHook.sh

--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -2,5 +2,5 @@
 set -e
 sudo npm update -y
 sudo pm2 update
-sudo aws s3 cp s3://practiceforcicd/practiceforcicd/pm2.config.js /home/ubuntu/sample-project/
+sudo aws s3 cp s3://practiceforcicd/practiceforcicd/config.js /home/ubuntu/sample-project/
 sudo service nginx reload
