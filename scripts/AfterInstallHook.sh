a872aeb95b0d0b91e1a2485f50ccd3ab9f04dc97
sanghavi2431 <sanghavi2431@gmail.com>
2024-11-06 15:36:23 +0530

    Update AfterInstallHook.sh

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+sudo pkill -f "node /home/ubuntu/sample-project/bin/app.js"
+sudo pkill -f "node /home/ubuntu/sample-project-1/bin/app.js"
 cd /home/ubuntu/sample-project-1 
 npm install
 npm run build
