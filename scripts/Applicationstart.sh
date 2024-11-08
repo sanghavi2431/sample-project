0d2ce8fe630b10718ed6c91d895fcf4d9ccb2f7f
sanghavi2431 <sanghavi2431@gmail.com>
2024-11-08 12:28:57 +0530

    Update Applicationstart.sh

--- scripts/Applicationstart.sh
+++ scripts/Applicationstart.sh
@@ -10,4 +10,13 @@ if [ $? != "0" ]; then
 else
    echo "PM2 started";
 fi
+
+echo "Restarting PM2 process: sample-project-1"
+pm2 restart "sample-project-1"
+if [ $? != "0" ]; then
+    cat /var/log/deployment-logs/bootscript-log.log;
+    error_exit "PM2 restart unsuccessful"
+else
+    echo "PM2 process restarted successfully"
+fi
 exit 0
