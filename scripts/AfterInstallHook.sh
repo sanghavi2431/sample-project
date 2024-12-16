91dad55f2b1776f4b7fbe806ac5000c3a63f217a
sanghavi2431 <sanghavi2431@gmail.com>
2024-12-16 21:29:39 +0530

    Update AfterInstallHook.sh

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -e
 sudo chown -R ubuntu:ubuntu /home/ubuntu/sample-project-1
-PIDS=$(ps -aef | grep "node /home/ubuntu/sample-project*/bin/app.js" | grep -v grep | awk '{print $2}')
-if [ -n "$PIDS" ]; then
-    for PID in $PIDS; do
-        sudo kill -9 $PID
-        echo "Killed Node server with PID $PID"
-    done
-else
-    echo "No Node server processes found."
-fi
+pm2 stop all
 cd /home/ubuntu/sample-project-1 
 npm install
 npm run build
